eeac4ec9cb7ca079137489a4a138dd5ffde7defb
Congfei <saintazunya@sjtu.edu.cn>
2018-11-30 16:31:51 -0600

    - added mnist example

--- src/tcn.py
+++ src/tcn.py
@@ -1,23 +1,28 @@
 import tensorflow as tf
 import numpy as np
-def tcn_layer(input, num_layers):
+def tcn_layer(input, num_layers,**params):
     '''
     this implmentation is able to deal with any dimension of inputs, as long as it's in
     [N, spatial features, C] format
 
     :param input: should be a seq tensor [N,length,data]
     :param num_layers:number of tcn layers
+    :param params:number of tcn layers,valid inputs are:
+            out_channels
     :return:
     '''
-    out_channels=[16]*num_layers
+    seq_len = input.get_shape()[1]
+    out_channels=params.get('num_filters',[8]*num_layers)
+    filter_size=params.get('filter_size',max(int(seq_len)//10,2))
+
     output=input
     for layer_num in range(num_layers):
         spatial_shape=input.get_shape()[1:-1]
-        filter_shape=[3,*spatial_shape[1:]]
+        filter_shape=[filter_size,*spatial_shape[1:]]
         input_channel=input.get_shape()[-1]
         out_channel=out_channels[layer_num]
         dilation_rate=1
-        seq_len=input.get_shape()[1]
+
         filter_variable=tf.get_variable('tcn_layer'+str(layer_num),filter_shape+[input_channel,out_channel],
                         tf.float32, tf.random_normal_initializer(0, 0.01),
                                     trainable=True)
@@ -29,7 +34,6 @@ def tcn_layer(input, num_layers):
         output=tf.nn.convolution(input,filter_variable,'VALID',dilation_rate=[dilation_rate]+[1]*(len(spatial_shape)-1))
         output=tf.layers.batch_normalization(output,axis=-1, training=True)
         output=tf.nn.relu(output)
-
         output=tf.layers.Dropout()(output)
         input=output
 
